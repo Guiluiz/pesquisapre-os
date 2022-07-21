95fb7dd1b6ddf85b983cb747fda8fb4cdfa8da58
Guiluiz <guilherme.luizcosta@gmail.com>
2022-07-21 15:21:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,28 @@
-function home() {
-    return <div>HOME1</div>
+
+import { useState } from 'react';
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
-export default home
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Guilherme', 'Margaret Hamilton'];
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Shipz. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
